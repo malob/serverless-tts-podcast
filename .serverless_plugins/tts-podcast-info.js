ba80b8a4bcb7419d352fc00dad6e2b1168a22fa1
Malo Bourgon <mbourgon@gmail.com>
2019-07-17 12:30:49 -0700

    Add display of podcast url in serverless info

--- .serverless_plugins/tts-podcast-info.js
+++ .serverless_plugins/tts-podcast-info.js
@@ -11,17 +11,21 @@ const conf = require('../config.json')
 // https://mrcoles.com/bookmarklet/
 const bookmarklet = `javascript:(function()%7Bvar%20url%20%3D%20btoa(window.location.href)%3Bvar%20xhr%20%3D%20new%20XMLHttpRequest()%3Bxhr.open('POST'%2C%20'https%3A%2F%2Fpubsub.googleapis.com%2Fv1%2Fprojects%2F${conf.gcp.project}%2Ftopics%2F${conf.gcp.parserPubSubTopic}%3Apublish%3Fkey%3D${conf.gcp.pubSubApiKey}'%2C%20true)%3Bxhr.setRequestHeader('Content-Type'%2C%20'application%2Fjson')%3Bxhr.send('%7B%20%22messages%22%3A%20%5B%7B%20%22data%22%3A%20%22'%20%2B%20url%20%2B%20'%22%20%7D%5D%20%7D')%7D)()`
 
-class Bookmarklet {
+class TtsPodcastInfo {
  constructor() {
     this.hooks = {
-      'info:info': this.displayBookmarklet,
+      'info:info': this.displayInfo,
     };
   }
 
-  displayBookmarklet() {
+  displayInfo() {
     console.log(chalk.green.underline('Bookmarklet'));
-    console.log(bookmarklet);
+    console.log(`${bookmarklet}\n`);
+
+
+    console.log(chalk.green.underline('Podcast URL'));
+    console.log(`http://storage.googleapis.com/podcasts/${conf.gcp.bucket}/podcast.xml`)
   }
 }
 
-module.exports = Bookmarklet;
+module.exports = TtsPodcastInfo;
